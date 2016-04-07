036a007719331c809f805cb556f29db5e7bb42e8
julianx2rl <julian.arguedastorres@gmail.com>
2016-04-07 14:42:42 -0600

    Added files via upload

---
+++ Baraja.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "Baraja.h"
+
+
+Baraja::Baraja()
+{
+	tamañoBaraja = 52;
+
+}//Fin de Constructor de Baraja
+
+
+Carta* Baraja::getCarta() {
+
+	Carta* respuesta = new Carta;
+	return respuesta;
+}//Fin de getCarta
+
+void Baraja::barajarCartas() {
+
+
+}//Fin de barajarCartas
+
+
+Baraja::~Baraja()
+{
+
+	
+}// Fin de destructor de Baraja.
